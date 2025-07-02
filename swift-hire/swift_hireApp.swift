b58a4dd332000f4fe51ac21f73c265b8110a84af
Tarun Bardawa <tarunbardawa3@gmail.com>
2025-07-02 10:37:32 +0530

    Initial Commit

---
+++ swift-hire/swift_hireApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_hireApp.swift
+//  swift-hire
+//
+//  Created by Tarun Bardawa on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_hireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
